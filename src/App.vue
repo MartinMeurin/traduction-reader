67617b8890cecd837d8362f9af7aad1a30e6aadd
Martin <martin.meurin@link-value.fr>
2020-06-24 19:27:20 +0200

    Import one file es-ES.json & populate cols

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,151 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <div id="app">
+  <div class="modal fade" id="addItemModal" tabindex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <form class="needs-validation" novalidate>
+          <div class="modal-header">
+            <h5 class="modal-title" id="modalLabel">Add traduction key</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <div class="form-group">
+              <label for="Inputlabel">Label</label>
+              <input type="texte" class="form-control" id="Inputlabel" aria-describedby="labelHelp" required>
+              <small id="labelHelp" class="form-text text-muted">You can use "/" to add colonne</small>
+              <div class="invalid-feedback" id="labelInvalid">
+                The label is missing 
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="InputContent">Content</label>
+              <input type="texte" class="form-control" id="InputContent" required>
+              <div class="invalid-feedback" id="contentInvalid">
+                The content is missing
+              </div>
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <!--<button type="button" class="btn btn-primary"  v-on:click="validForm">Save changes</button>-->
+          </div>
+        </form>
+      </div>
+    </div>
   </div>
+  <div class="container-fluid">
+    <div class="row">
+      <col-item v-for='col in cols' v-bind:col='col' v-bind:key="col.id"></col-item>
+    </div>
+  </div>
+  <div class="btn-group">
+    <button type="button" class="btn btn-langue dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+      <!--{{currentLanguage.name}}-->
+    </button>
+    <div class="dropdown-menu">
+      <!--<translate-file v-for='file in fileData' v-bind:translatefile='file' v-bind:key="file.id"></translate-file>-->
+    </div>
+  </div>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import ColItem from './components/col-item'
+  import data from './data/es-ES.json'
+  import feather from 'feather-icons'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    components: {
+      ColItem
+    },
+    data(){
+      return{
+        cols:[],
+        fileData:[],
+        currentLanguage:{}
+      }
+    },
+    methods: {
+      ///
+      organiseDataStructure(jsonData){
+        let data = {};
+        for (var value in jsonData){
+          if (!Array.isArray(jsonData[value]) || typeof jsonData[value] !== 'object') {
+            this.setItem(data, value.split('.'), jsonData[value]);
+          }
+        }
+        return data;
+      },
+      setItem(data, dataSplit,value){
+        let items = data;
+        for(var i=0; i<dataSplit.length; i++){
+          let label = dataSplit[i];
+          if(typeof items[label] !== 'undefined' &&  typeof items[label] !== 'string'){ items = items[label]; }
+          else {
+            if( i == dataSplit.length-1 ){
+              items[label] = value;
+            }else{
+              items[label] = { };
+              items = items[label];
+            }
+          }
+        }
+      },
+      createCol(data,idCol){
+        for(let i=0; i<this.cols.length; i++){
+          this.cols[i].active = false;
+        }
+        let itemsCol = this.getDataCol(data);
+
+        //create
+        if(idCol === undefined) this.cols.push({ "id":(this.cols.length), "active":false, "value":itemsCol });
+        //updateData ---- /!\ tester si j'ai le même jeux de data /!\
+        if(idCol != undefined){ 
+          this.cols[idCol].value = itemsCol;
+        }
+
+        this.cols[this.cols.length-1].active = true;
+        this.$nextTick(() => { 
+          feather.replace();
+          //jQuery('[data-toggle="tooltip"]').tooltip();
+        })
+      },
+      getDataCol(data){
+        let itemsCol = [];
+        let endItems = [];
+        let item = '';
+        let count = 0;
+        for(let label in data){
+          let idItem = this.cols.length+""+count+""+label;
+          item = { "id":idItem, "position":count, "label":label, "current":false, "active":false }
+          if(typeof data[label] === 'string'){ 
+            item.value = data[label]; 
+            endItems.push(item);
+          } else {
+            itemsCol.push(item);
+            count++;
+          }
+          
+        }
+        // place end item to the end of the list
+        for(let i=0; i < endItems.length; i++){
+          endItems[i].id = count++;
+          itemsCol.push( endItems[i] );
+        }
+        return itemsCol;
+      },
+      ///
+      initApp(){
+        this.currentLanguage = this.fileData[0];
+        this.createCol({...this.currentLanguage.dataSetOrganise});
+      }
+    },
+    created(){
+      this.fileData.push({ id:0, name:'es-ES', extension:'json', dataSet:data, dataSetOrganise:this.organiseDataStructure(data)})
+      this.initApp();
+    }
   }
-}
 </script>
---
+++ src/components/col-item.vue
@@ -0,0 +1,46 @@
+<template>
+  <nav v-bind:class="{ active:col.active }" v-on:click="clickOnNav" class="col-md-2 d-none d-md-block sidebar bg-light">
+    <div class="sidebar-sticky">
+      <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-4 text-muted">
+        <span>col-{{col.id}}</span>
+        <button type="button" class="" data-toggle="modal" data-target="#addItemModal" v-on:click="addModale"><span data-feather="plus"></span></button>
+      </h6>
+      <ul class="nav flex-column mb-2">
+        <!--<translate-item v-for="value in col.value" v-bind:translateitem="value" v-bind:col="col.id" v-bind:key="value.id"></translate-item>-->
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+  export default{
+    props:{
+      col:Object
+    },
+    methods:{
+      clickOnNav() {
+        /*app.statusClick.col = this.col.id;
+        if(!app.statusClick.button){
+          app.navigate();
+        }
+        app.resetStatusClick();*/
+      },
+      addModale(){
+        /*app.statusClick.col = this.col.id;
+        app.resetStatusInput();
+        // Préremplir le champ
+        let val = '/';
+        for(var i=0; i<app.cols.length-1; i++){
+          val += app.cols[i].selected.label+'/'; 
+        }
+        document.getElementById('Inputlabel').value = val;
+        document.getElementById('InputContent').value = '';
+        window.setTimeout(function () {
+            document.getElementById("Inputlabel").focus();
+        },1000);
+        */
+      },
+    }
+  }
+
+</script>
