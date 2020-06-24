ce5ccf037556ac64660bbc85b3536d6f1df275b8
Martin <martin.meurin@link-value.fr>
2020-06-24 18:37:42 +0200

    FEAT!: VueCLI project

--- api/dir.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$out = array();
-foreach (glob($_GET['folderUrl'].'*') as $filename) {
-    $p = pathinfo($filename);
-    $out[] = $p['basename'];
-}
-
-echo json_encode($out); 
-?>
--- src/main.js
+++ src/main.js
@@ -1,15 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import 'bootstrap/dist/css/bootstrap.css'
 
 Vue.config.productionTip = false
 
-window.$ = window.jQuery = require('jquery');
-
 new Vue({
-	data:{
-		statusClick:{ col:false , button:false },
-	},
-	render: h => h(App),
-
+  render: h => h(App),
 }).$mount('#app')
